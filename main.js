fb794da4c7348f1589475ea926a2060f87863864
maaqoul <hicham.maaqoul@gmail.com>
2018-10-16 14:43:29 +0100

    removing the decimal number for the ounce converter

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ lbsInput.addEventListener('input', function(event) {
     let pounds = event.target.value;
     document.getElementById('gramsOutputs').innerHTML = (pounds/0.0022046).toFixed(2);
     document.getElementById('kgOutputs').innerHTML = (pounds/2.2046).toFixed(2);
-    document.getElementById('ozOutputs').innerHTML = (pounds*16).toFixed(2);
+    document.getElementById('ozOutputs').innerHTML = (pounds*16);
 });
 
 
